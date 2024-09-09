b7219a003a4d9c303824b09bffa8e15ce1791971
julzybae <juliogerald25@gmail.com>
2024-09-10 02:34:17 +0700

    feat: animate per section

--- js/script.js
+++ js/script.js
@@ -23,3 +23,21 @@ const menuList = document.getElementById("menu-list");
 menuIcon.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
 });
+
+// Animate
+let sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 200;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    }
+    // else {
+    //   sec.classList.remove("show-animate");
+    // }
+  });
+};
